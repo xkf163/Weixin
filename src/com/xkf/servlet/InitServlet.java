d07757dec4ab3d451193b9a816e62ce570afc8e5
xkf163 <572571573@qq.com>
2016-05-25 21:28:07 +0800

    20150525 mod

--- src/com/xkf/servlet/InitServlet.java
+++ src/com/xkf/servlet/InitServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
+
 public class InitServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
